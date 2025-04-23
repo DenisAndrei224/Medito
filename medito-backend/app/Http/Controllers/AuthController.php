cc11f9627de8a9ad627616c01497d66c47d62dbd
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-04-23 14:39:09 +0300

    Add authentication controller

---
+++ medito-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed' // password_confirmation required in the post request
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        // This will be used when building an SPA, now we're building an API
+        // Auth::attempt([
+        //     'email' => $email,
+        //     'password' => $password
+        // ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        // If the user doesn't exist or the password doesn't correspond
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return [
+                'message' => 'The provided credentials are incorrect.'
+            ];
+        }
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+
+    }
+
+    public function logout(Request $request) {
+        return 'logout';
+    }
+}
