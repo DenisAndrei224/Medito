858eab3a5439879ce449d1f83b1ccee9092219cc
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-04-29 15:01:29 +0300

    Add middleware to protect certain methods and change store to create a post through a user

--- medito-backend/app/Http/Controllers/PostController.php
+++ medito-backend/app/Http/Controllers/PostController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -31,7 +35,8 @@ class PostController extends Controller
             'body' => 'required'
         ]);
 
-        $post = Post::create($fields);
+        // Creating a post through a user (the method is looking for an authenticated user)
+        $post = $request->user()->posts()->create($fields);
 
         return $post;
     }
@@ -56,6 +61,8 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        Gate::authorize('modify', $post);
+
         $fields = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required'
@@ -74,6 +81,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        Gate::authorize('modify', $post);
+        
         $post->delete();
         return ['message' => 'The post was deleted!'];
     }
