ed59229aef6fc6c8a1832e8c5a6a0826c73eb41f
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-04-29 15:04:45 +0300

    Add foreign key from post to user

--- medito-backend/database/migrations/2025_04_23_100614_create_posts_table.php
+++ medito-backend/database/migrations/2025_04_23_100614_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();            $table->string('title');
             $table->text('body');
             $table->timestamps();
         });
