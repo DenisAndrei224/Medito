df309c28a87d6e3b3763a758dcab9bc409d9ad96
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-04-29 15:05:56 +0300

    Implement get request to get user if its authenticated

--- medito-backend/routes/api.php
+++ medito-backend/routes/api.php
@@ -20,6 +20,11 @@ use App\Http\Controllers\PostController;
 //     return $request->user();
 // });
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
 Route::apiResource('posts', PostController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
