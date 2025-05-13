c1fa40a4071d88d7997dccdc85a715d31eabf43e
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-05-13 17:28:11 +0300

    Register form created

--- medito-frontend/src/app/register/register.component.ts
+++ medito-frontend/src/app/register/register.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  form: any = {
+    fullName: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+    role: null,
+  };
 
-  constructor() { }
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onSelectChange(event: any) {
+    console.log('Selected value:', event.target.value);
+    this.form.role = event.target.value;
   }
 
+  onSubmit(): void {
+    const { fullName, email, password, password_confirmation, role } =
+      this.form;
+
+    this.authService
+      .register(fullName, email, password, password_confirmation, role)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.isSuccessful = true;
+          this.isSignUpFailed = false;
+        },
+        error: (err) => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        },
+      });
+    console.log('User registered successfully!');
+    this.router.navigate(['/']);
+  }
 }
