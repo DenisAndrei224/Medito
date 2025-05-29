93f3ce08a6135fc64e366f2dd24ac4db6711ef25
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-05-29 16:57:30 +0300

    Navbar now knows user logged in

--- medito-frontend/src/app/navbar/navbar.component.ts
+++ medito-frontend/src/app/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+  currentUser: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.currentUser.subscribe((user) => {
+      this.isLoggedIn = !!user;
+      this.currentUser = user;
+    });
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
