eae913ba299f5bc26affc48fb54a5f68a418707e
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-06-03 01:07:26 +0300

    Slightly change routes to add user controller

--- medito-backend/routes/api.php
+++ medito-backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +21,33 @@ use App\Http\Controllers\PostController;
 //     return $request->user();
 // });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::apiResource('posts', PostController::class);
-
+// Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // User routes
+    Route::prefix('user')->group(function () {
+        Route::get('/', function (Request $request) {
+            return $request->user();
+        });
+        
+        // Profile update route
+        Route::patch('/profile', [UserController::class, 'updateProfile']);
+        
+        // Avatar upload route
+        Route::post('/avatar', [UserController::class, 'updateAvatar']);
+    });
+    
+    // Logout route
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Posts resource
+    Route::apiResource('posts', PostController::class);
+});
