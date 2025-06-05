5884adea960d0429c35d6d9d5a0aeec3989436a2
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-06-05 16:12:47 +0300

    Add posts route

--- medito-frontend/src/app/app-routing.module.ts
+++ medito-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { AuthGuard } from './auth/auth.guard';
+import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {
+    path: 'posts',
+    children: [
+      { path: '', component: PostListComponent },
+      {
+        path: 'create',
+        component: CreatePostComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
 ];
 
 @NgModule({
