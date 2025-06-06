db62d14ae791376fb4645065d3ebaa3344e4f90f
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-06-06 22:11:47 +0300

    Modified app module

--- medito-frontend/src/app/app.module.ts
+++ medito-frontend/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { AuthInterceptor } from './services/auth.interceptor';
 import { ProfileComponent } from './profile/profile.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { MyRequestsComponent } from './my-requests/my-requests.component';
+import { MyTeacherPageComponent } from './pages/my-teacher-page/my-teacher-page.component';
+import { ManageResourcesComponent } from './manage-resources/manage-resources/manage-resources.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +27,10 @@ import { PostListComponent } from './post-list/post-list.component';
     ProfileComponent,
     CreatePostComponent,
     PostListComponent,
+    PostDetailComponent,
+    MyRequestsComponent,
+    MyTeacherPageComponent,
+    ManageResourcesComponent,
   ],
   imports: [
     BrowserModule,
