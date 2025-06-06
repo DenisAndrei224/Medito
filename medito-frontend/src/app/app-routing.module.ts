7bb52cc088e8a8a8c10020b7692057cd570bb541
Denis Firulescu <firulescu.denis.314k@gmail.com>
2025-06-06 22:11:28 +0300

    Add new routes

--- medito-frontend/src/app/app-routing.module.ts
+++ medito-frontend/src/app/app-routing.module.ts
@@ -5,22 +5,43 @@ import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TeacherGuard } from './teacher.guard';
 import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { MyRequestsComponent } from './my-requests/my-requests.component';
+import { MyTeacherPageComponent } from './pages/my-teacher-page/my-teacher-page.component';
+import { ManageResourcesComponent } from './manage-resources/manage-resources/manage-resources.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   {
+    path: 'my-requests',
+    component: MyRequestsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'my-teacher',
+    component: MyTeacherPageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
     path: 'posts',
     children: [
       { path: '', component: PostListComponent },
+      { path: ':id', component: PostDetailComponent },
       {
         path: 'create',
         component: CreatePostComponent,
         canActivate: [AuthGuard],
       },
     ],
+  },
+  {
+    path: 'teacher/manage-resources',
+    component: ManageResourcesComponent,
+    canActivate: [AuthGuard, TeacherGuard], // User must be logged in AND be a teacher
   },
 ];
 
